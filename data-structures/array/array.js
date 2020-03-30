9a93d644535fe85320a662f9806088ea1776765c
Anthony Morris <anthony@amorrissound.com>
2020-03-30 09:24:32 -0700

    Add comments to array

--- data-structures/array/array.js
+++ data-structures/array/array.js
@@ -11,6 +11,8 @@ class Array {
 
   increaseSize() {
     this.arrSize++;
+
+    // Double the array when we have run out of capacity
     if (this.arrSize === this.arrCapacity) {
       this.resize(this.arrSize * 2);
     }
@@ -18,6 +20,8 @@ class Array {
 
   decreaseSize() {
     this.arrSize--;
+
+    // Half the capacity of the array if it is only 1/4 full
     if (this.arrSize === this.arrCapacity / 4) {
       this.resize(this.arrCapacity / 2);
     }
@@ -37,9 +41,11 @@ class Array {
 
   at(index) {
     const item = this.data[index];
+
     if (!item) {
       throw Error('Index out of bounds');
     }
+
     return item;
   }
 
@@ -50,9 +56,12 @@ class Array {
   }
 
   insert(index, item) {
+    // Shuffle every item down so we can insert 
+    // a new item into the array
     for (let i = this.arrSize; i >= index; i--) {
       this.data[i + 1] = this.data[i];
 
+      // Index found, add the item
       if (i === index) {
         this.data[i] = item;
         break;
@@ -65,41 +74,57 @@ class Array {
   }
 
   prepend(item) {
+    // Use internal insert method
+    // to insert at the front of the array
     this.insert(0, item);
     return this.arrSize;
   }
 
   pop() {
+    // Remove the last item of the array
     const item = this.data[this.arrSize - 1];
     this.data[this.arrSize - 1] = null;
+
     this.decreaseSize();
+
     return item;
   }
 
   delete(index) {
+    // Shuffle all items down to the index
     for (let i = index + 1; i <= this.arrSize - 1; i++) {
       this.data[i - 1] = this.data[i];
     }
+
+    // Remove the last item which is invalid
     delete this.data[this.arrSize - 1];
+
     this.decreaseSize();
+
     return this.arrSize;
   }
 
   remove(item) {
+    // Find the correct item then use the internal
+    // delete method to shuffle everything down
     for (let i = 0; i < this.arrSize - 1; i++) {
       if (this.data[i] === item) {
         this.delete(i);
       }
     }
+
     return this.arrSize;
   }
 
   find(item) {
+    // Return the index of the item if found
+    // otherwise -1
     for (let i = 0; i < this.arrSize - 1; i++) {
       if (this.data[i] === item) {
         return i;
       }
     }
+
     return -1;
   }
 }
