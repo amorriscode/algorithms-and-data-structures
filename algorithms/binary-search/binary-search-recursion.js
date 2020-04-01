700721b5aa60a23fa93b89fa727dee14ffcb9afc
Anthony Morris <anthony@amorrissound.com>
2020-04-01 10:46:35 -0700

    Add rounding explanation

--- algorithms/binary-search/binary-search-recursion.js
+++ algorithms/binary-search/binary-search-recursion.js
@@ -3,6 +3,11 @@ const binarySearch = (array, value, min = 0, max = array.length - 1) => {
     return -1;
   }
 
+  // Avoid rounding bug
+  // Division truncates; when max + min is negative
+  // it would start rounding toward the higher bound.
+  // We ensure the number is always positive and will
+  // always round to the lower bound.
   const guess = min + Math.floor((max - min) / 2);
 
   if (array[guess] === value) {
--- algorithms/binary-search/binary-search.js
+++ algorithms/binary-search/binary-search.js
@@ -4,6 +4,11 @@ const binarySearch = (array, value) => {
   let guess;
 
   while (min <= max) {
+    // Avoid rounding bug
+    // Division truncates; when max + min is negative
+    // it would start rounding toward the higher bound.
+    // We ensure the number is always positive and will
+    // always round to the lower bound.
     guess = min + Math.floor((max - min) / 2);
 
     if (array[guess] === value) {
